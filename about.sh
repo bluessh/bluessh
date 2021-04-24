068ea1b38dbb269ac5dcce80482e200b342f54d5
bluessh <83107224+bluessh@users.noreply.github.com>
2021-04-24 22:25:47 +0800

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+clear
+echo -e "================================================="
+echo -e "#  Premium Auto Script By Adjie Poetra Pratama  #"
+echo -e "#-----------------------------------------------#"
+echo -e "# For Debian 9 & Debian 10 64 bit               #"
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #"
+echo -e "# For VPS with KVM and VMWare virtualization    #"
+echo -e "# Build Up By Adjie Poetra Pratama              #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Thanks To                                     #"
+echo -e "#-----------------------------------------------#"
+echo -e "# Adjieptra                                     #"
+echo -e "# Allah SWT                                     #"
+echo -e "# My Family                                     #"
+echo -e "# Admin BLUESSH & VPNSTORES                     #"
+echo -e "# BlueSSH                                       #"
+echo -e "# Horass                                        #"
+echo -e "# Vinstechmy                                    #"
+echo -e "#-----------------------------------------------#"
+echo -e "================================================="
